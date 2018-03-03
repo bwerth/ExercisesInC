59fd1ceb18847a2ff5071d8363ec7c8af00005ab
Bryan Werth <bryan.werth@students.olin.edu>
2018-03-03 10:18:52 -0500

    Finished Homework 4

---
+++ exercises/ex04/trout.h
@@ -0,0 +1,56 @@
+#ifndef TROUT_H_
+#define TROUT_H_
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <time.h>
+#include <netinet/in.h>
+#include <netinet/in_systm.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+#include <netinet/udp.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <sys/un.h>
+#include <stdarg.h>
+#include <syslog.h>
+#include <unistd.h>
+
+#define BUFSIZE 1500
+
+typedef struct rec {                /* outgoing UDP data */
+  u_short seq;          /* sequence number */
+} Rec;
+
+typedef struct timeval Timeval;
+typedef struct sockaddr Sockaddr;
+
+extern int max_ttl;
+extern char sendbuf[BUFSIZE];
+extern Rec *rec;
+extern int datalen;
+extern Sockaddr *sasend;    /* socket addresses for various purposes */
+extern socklen_t salen;                    /* length of a socket address */
+extern Sockaddr *sarecv;
+extern Sockaddr *salast;
+extern Sockaddr *sabind;
+
+/* the following are a few definitions from Stevens' unp.h */
+
+#define max(a,b) ((a) > (b) ? (a) : (b))
+
+/* the following are prototypes for the Stevens utilities in util.c */
+
+
+void Sendto(int fd, const void *ptr, size_t nbytes, int flags,
+	    const struct sockaddr *sa, socklen_t salen);
+void loop_ttl();
+
+#endif
